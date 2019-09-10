abddb090069bde6c1a6957debcfaa8a9170e62ff
Max calderon <maxvalentino@hotmail.com>
2019-09-09 21:39:06 -0500

    commit

---
+++ Practica1/Assets/Agent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Agent : MonoBehaviour
+{
+    Vector3 velocity = Vector3.zero;
+    Vector3 desired = Vector3.zero;
+    Vector3 steer = Vector3.zero;
+    float maxSpeed = 5;
+    float maxSteer = 5;
+
+    Transform target;
+
+    void Update(){
+        target = GameObject.Find("Target").transform;
+
+        desired = (target.position - transform.position).normalized * maxSpeed;
+        //desired = -(target.position - transform.position).normalized * maxSpeed;
+        steer = Vector3.ClampMagnitude(desired - velocity, maxSteer);
+
+        //velocity += steer;
+        velocity += steer * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+        
+        if(transform.position.x <= -7.6 || transform.position.y >= 7.6 || transform.position.x <= -2.06 || transform.position.y >= 6.84) 
+        {   
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Destroy(gameObject);
+            Debug.Log("Destruido");
+            
+        }
+    } 
+}
---
+++ Practica1/Assets/Target.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{   
+    //float tx = 0;
+    //float ty = 0;
+    //float ty = 20;
+//    float minX = -5;
+    //float withX = -5;
+//    float width = 10;
+//    float height = 10; 
+    void Update(){
+        //ty += 0.01f;
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.Scale(new Vector3(1, 1, 0));
+        transform.position = mousePosition;
+        //tx += 0.01f;
+        //ty += 0.01f;        
+       // float x = 10 * Mathf.PerlinNoise(tx, 0);
+        //float y = 10 * Mathf.PerlinNoise(ty, 0);
+        //transform.position = new Vector3(x, y, 0);
+        //tx += 0.01f;
+        //float x = -5 + 10 * Mathf.PerlinNoise(tx, 0);
+       // float y = -5 + 10 * Mathf.PerlinNoise(0, ty);
+        //transform.position = new Vector3(x, y, 0); 
+    }
+}
